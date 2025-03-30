ec8c2d95e193426ff880f4df6bd83af6cfc4552b
zmuranaka <zmuranaka@gmail.com>
2025-03-30 12:28:58 -0600

    Rename incorrectly named lib/magic_the_gathering_flutter.dart, add exports to it * This should _not_ be a breaking change for anyone because previously the library file wasn't exporting anything, so nobody would have reason to be importing the file directly

---
+++ lib/scryfall_api_symbols.dart
@@ -0,0 +1,8 @@
+/// Extensions for the [scryfall_api](https://pub.dev/packages/scryfall_api)
+/// package that simplify displaying Magic: The Gathering symbols as Flutter
+/// widgets.
+library;
+
+export 'extensions/prepared_mana_cost.dart';
+export 'extensions/prepared_oracle_text.dart';
+export 'models/mtg_symbology.dart';
